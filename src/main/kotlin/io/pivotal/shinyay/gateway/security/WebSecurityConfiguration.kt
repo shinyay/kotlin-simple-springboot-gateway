a3ad6f7e46c70ff8b7d57e5c839bbd36bc9ec899
shinyay <shinya.com@gmail.com>
2018-08-29 21:19:05 +0900

    Add annotation which is not put

--- src/main/kotlin/io/pivotal/shinyay/gateway/security/WebSecurityConfiguration.kt
+++ src/main/kotlin/io/pivotal/shinyay/gateway/security/WebSecurityConfiguration.kt
@@ -1,10 +1,16 @@
 package io.pivotal.shinyay.gateway.security
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
+@Configuration
+@EnableWebSecurity
+@Order(1)
 class WebSecurityConfiguration : WebSecurityConfigurerAdapter() {
 
     @Autowired
